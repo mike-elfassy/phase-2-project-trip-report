9289595ff0618c6691651ac74de8760fb703c353
Michael Elfassy <mike.elfassy@gmail.com>
2023-05-29 12:26:22 -0400

    Added Material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button
+        
+          >
+          Hello World
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
